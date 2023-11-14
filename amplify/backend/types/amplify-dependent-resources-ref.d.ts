17af3dc114de93e797e8db11100a9fe39ce35e5a
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-11-13 20:46:38 -0800

    Add GraphQL and S3 storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "noteimage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
